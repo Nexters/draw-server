3b60465ca0c57e8ea72e989ec828d766265856f2
giyeon95 <giyeon15@gmail.com>
2023-08-05 13:35:27 +0900

    fix: 중복 cors 설정 제거 & swagger 내 서버 URL 설정

--- src/main/kotlin/com/draw/config/OpenApiConfig.kt
+++ src/main/kotlin/com/draw/config/OpenApiConfig.kt
@@ -5,13 +5,23 @@ import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
+import io.swagger.v3.oas.models.servers.Server
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
 
 @Configuration
 class OpenApiConfig {
+
     @Bean
-    fun openAPI(): OpenAPI {
+    fun openAPI(env: Environment): OpenAPI {
+
+        val swaggerUrl =
+            if (env.activeProfiles.contains("prod")) {
+                "https://draw-nexters.kro.kr"
+            } else {
+                "http://localhost:8080"
+            }
 
         return OpenAPI()
             .components(
@@ -22,7 +32,10 @@ class OpenApiConfig {
                             .type(SecurityScheme.Type.HTTP)
                             .scheme("bearer").bearerFormat("JWT")
                     )
-            ).addSecurityItem(
+            ).addServersItem(
+                Server().url(swaggerUrl)
+            )
+            .addSecurityItem(
                 SecurityRequirement().addList("Authorization")
             ).info(
                 Info()
--- src/main/kotlin/com/draw/config/security/SecurityConfig.kt
+++ src/main/kotlin/com/draw/config/security/SecurityConfig.kt
@@ -10,9 +10,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.CorsConfigurationSource
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
@@ -26,7 +23,6 @@ class SecurityConfig(
     fun apiFilterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
         return httpSecurity
             .csrf { csrf -> csrf.disable() }
-            .cors { cors -> cors.configurationSource(corsConfigurationSource()) }
             .httpBasic { httpBasic -> httpBasic.disable() }
             .formLogin { formLogin -> formLogin.disable() }
             .sessionManagement { mgmt -> mgmt.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
@@ -49,7 +45,6 @@ class SecurityConfig(
     fun nonBearerAuthorizationFilterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
         return httpSecurity
             .csrf { csrf -> csrf.disable() }
-            .cors { cors -> cors.configurationSource(corsConfigurationSource()) }
             .httpBasic { httpBasic -> httpBasic.disable() }
             .formLogin { formLogin -> formLogin.disable() }
             .sessionManagement { mgmt -> mgmt.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
@@ -57,21 +52,6 @@ class SecurityConfig(
                 it.anyRequest().permitAll()
             }
             .build()
-    }
-
-    @Bean
-    fun corsConfigurationSource(): CorsConfigurationSource {
-        val config = CorsConfiguration().apply {
-            allowCredentials = true
-            allowedOrigins = listOf("http://localhost:3000", "https://draw-nexters.kro.kr")
-            allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-            allowedHeaders = listOf("Authorization", "Cache-Control", "Content-Type")
-            exposedHeaders = listOf("*")
-        }
-
-        val source = UrlBasedCorsConfigurationSource()
-        source.registerCorsConfiguration("/**", config)
-        return source
     }
 
     private fun permitAllUrls(): Array<String> {
