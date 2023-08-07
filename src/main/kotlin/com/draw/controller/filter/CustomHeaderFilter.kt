edd3966340446998fdaab5cb1dd237f68d1e4939
giyeon95 <giyeon15@gmail.com>
2023-08-08 00:55:50 +0900

    feat: 헤더 설정 추가

--- src/main/kotlin/com/draw/controller/filter/CustomHeaderFilter.kt
+++ src/main/kotlin/com/draw/controller/filter/CustomHeaderFilter.kt
@@ -13,6 +13,11 @@ class CustomHeaderFilter : OncePerRequestFilter() {
     ) {
         // TODO: cors 테스트 용도 2023/08/08 (koi)
         if (request.method == "OPTIONS") {
+            response.setHeader("Access-Control-Allow-Origin", "*")
+            response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+            response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept, X-Requested-With, remember-me")
+            response.setHeader("Access-Control-Max-Age", "3600")
+
             response.status = HttpServletResponse.SC_OK
             return
         }
