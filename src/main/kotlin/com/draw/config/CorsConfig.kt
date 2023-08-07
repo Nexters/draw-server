eced2fc8b6c3a3c78690e9290bba01b1ea200923
giyeon95 <giyeon15@gmail.com>
2023-08-07 22:06:56 +0900

    fix: 미사용 import 구문 제거

--- src/main/kotlin/com/draw/config/CorsConfig.kt
+++ src/main/kotlin/com/draw/config/CorsConfig.kt
@@ -1,6 +1,5 @@
 package com.draw.config
 
-import io.swagger.v3.oas.models.PathItem.HttpMethod
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
