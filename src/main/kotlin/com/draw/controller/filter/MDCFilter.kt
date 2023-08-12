a24ab869efe138a7b6123271ce183d92d20f0009
giyeon95 <giyeon15@gmail.com>
2023-08-13 01:18:53 +0900

    feat: 로깅 개선 및 Sentry 연동

--- src/main/kotlin/com/draw/controller/filter/MDCFilter.kt
+++ src/main/kotlin/com/draw/controller/filter/MDCFilter.kt
@@ -12,7 +12,6 @@ class MDCFilter : OncePerRequestFilter() {
     override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
         MDC.put(Const.REQUEST_ID, UUID.randomUUID().toString().substring(0, 8))
         MDC.put(Const.REQUEST_TIME, System.currentTimeMillis().toString())
-        MDC.put(Const.USER_ID, "todo") // TODO:  2023/07/19 (koi)
 
         try {
             filterChain.doFilter(request, response)
