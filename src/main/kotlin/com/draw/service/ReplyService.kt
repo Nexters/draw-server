6c73bfa6414fc63ba5d068b485395a71d54f8425
giyeon95 <giyeon15@gmail.com>
2023-08-12 22:07:35 +0900

    feat: peek 훔쳐보기 기능 개발

--- src/main/kotlin/com/draw/service/ReplyService.kt
+++ src/main/kotlin/com/draw/service/ReplyService.kt
@@ -1,10 +1,13 @@
 package com.draw.service
 
+import com.draw.common.BusinessException
 import com.draw.common.enums.ClaimOriginType
+import com.draw.common.enums.ErrorType
 import com.draw.common.enums.Gender
 import com.draw.common.enums.MBTI
 import com.draw.common.exception.FeedNotFoundException
 import com.draw.common.exception.ReplyNotFoundException
+import com.draw.common.exception.UserNotFoundException
 import com.draw.controller.dto.MyRepliesRes
 import com.draw.controller.dto.MyReplyRes
 import com.draw.controller.dto.RepliesRes
@@ -20,6 +23,7 @@ import com.draw.infra.persistence.ClaimRepository
 import com.draw.infra.persistence.FeedRepository
 import com.draw.infra.persistence.PeekReplyRepository
 import com.draw.infra.persistence.ReplyRepository
+import com.draw.infra.persistence.user.UserRepository
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -31,6 +35,7 @@ class ReplyService(
     private val replyRepository: ReplyRepository,
     private val peekReplyRepository: PeekReplyRepository,
     private val claimRepository: ClaimRepository,
+    private val userRepository: UserRepository,
 ) {
     fun getReplies(user: User?, feedId: Long): RepliesRes {
         val feed = feedRepository.findByIdOrNull(feedId) ?: throw FeedNotFoundException()
@@ -108,7 +113,18 @@ class ReplyService(
         val reply = replyRepository.findByIdOrNull(replyId) ?: throw ReplyNotFoundException()
         require(reply.writerId != user.id!!) { "Not allowed peek own reply" }
 
-        // TODO: 여기에서 포인트 제외 & peek 저장 & 관련 서비스로직 적용 필요 2023/07/24 (koi)
+        if (peekReplyRepository.existsByUserIdAndReply(user.id!!, reply)) {
+            throw BusinessException(ErrorType.ALREADY_PEEKED)
+        }
+
+        if (!user.canUsePoint(PEEK_POINT_TO_USE)) {
+            throw BusinessException(ErrorType.POINT_IS_NOT_ENOUGH)
+        }
+
+        user.usePoint(PEEK_POINT_TO_USE)
+        val writerUser = userRepository.findByIdOrNull(reply.writerId) ?: throw UserNotFoundException()
+        writerUser.addPoint(PEEK_POINT_TO_SUPPLY)
+
         peekReplyRepository.save(
             PeekReply(
                 userId = user.id!!,
@@ -116,7 +132,16 @@ class ReplyService(
             )
         )
 
-        return ReplyWriterRes(MBTI.ENFP, Gender.MALE, 29) // TODO:  2023/08/02 (koi)
+        userRepository.saveAll(
+            mutableListOf(
+                user,
+                writerUser,
+            )
+        )
+
+        // TODO: writerUser에게 push 발송 2023/08/12 (koi)
+
+        return ReplyWriterRes(writerUser.mbti, writerUser.gender, writerUser.getAge())
     }
 
     private fun Map<Reply, ReplyWriterRes>.getStatus(reply: Reply, user: User?) =
@@ -124,4 +149,9 @@ class ReplyService(
         else if (this.containsKey(reply)) ReplyStatus.PEEKED
         else if (reply.writerId == user.id) ReplyStatus.MINE
         else ReplyStatus.NORMAL
+
+    companion object {
+        private const val PEEK_POINT_TO_USE = 10L
+        private const val PEEK_POINT_TO_SUPPLY = 5L
+    }
 }
